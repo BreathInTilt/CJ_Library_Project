3cd3c7860413551e20c437919ea01dd032e626a5
BreathInTilt <you.path.e2e4@gmail.com>
2025-08-04 18:10:58 +0200

    Secret key is set by default, .env is no longer needed, requirements.txt is updated

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ import json
 #import plotly.express as px
 import plotly.graph_objects as go
 import plotly.offline as pyo
-from dotenv import load_dotenv
 
-load_dotenv()
 app = Flask(__name__)
-app.secret_key = os.getenv("SK")
+app.secret_key = "kapksdpakk2213"
 
 
 users = read_users()
